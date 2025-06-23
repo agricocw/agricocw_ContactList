85907f19c7ce8bada5f33277e48af2c8f01370e6
cwagr <agricocw@mail.uc.edu>
2025-06-22 21:59:32 -0400

    Add project files.

---
+++ agricocw_ContactList/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace agricocw_ContactList
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
---
+++ agricocw_ContactList/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace agricocw_ContactList
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
