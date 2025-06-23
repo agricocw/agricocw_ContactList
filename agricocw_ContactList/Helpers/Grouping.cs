12f56bc3cdd67bb5ef630b4185fdb7076db615ae
cwagr <agricocw@mail.uc.edu>
2025-06-22 22:06:20 -0400

    Progress so far

---
+++ agricocw_ContactList/Helpers/Grouping.cs
@@ -0,0 +1,9 @@
+﻿public class Grouping<K, T> : List<T>
+{
+    public K Name { get; private set; }
+
+    public Grouping(K name, IEnumerable<T> items) : base(items)
+    {
+        Name = name;
+    }
+}
---
+++ agricocw_ContactList/Models/Contact.cs
@@ -0,0 +1,11 @@
+﻿namespace agricocw_ContactList.Models
+{
+    public class Contact
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Description { get; set; }
+        public string Photo { get; set; }
+    }
+}
